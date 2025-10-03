e94132b6ab04f8de7d4ad83c02bdc06e3b89f698
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:16:03 +0530

    Create P77.C

---
+++ P77.C
@@ -0,0 +1,8 @@
+// Program 77: Print all ASCII characters from 65 to 90
+#include <stdio.h>
+int main() {
+    for(char ch = 65; ch <= 90; ch++) {
+        printf("%c ", ch);
+    }
+    return 0;
+}
