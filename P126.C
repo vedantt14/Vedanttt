4ff849fddf9d0cea9c9ba1675e41bcfc5cbc068d
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:54:00 +0530

    Create P126.C

---
+++ P126.C
@@ -0,0 +1,9 @@
+// 26. Area of triangle
+#include <stdio.h>
+int main() {
+    float b, h;
+    printf("Enter base and height: ");
+    scanf("%f%f", &b, &h);
+    printf("Area = %.2f", 0.5 * b * h);
+    return 0;
+}
