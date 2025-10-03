efe6e6498c4d9fb3780c70d3bc401a4387f7671d
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:31:31 +0530

    Create P87.C

---
+++ P87.C
@@ -0,0 +1,12 @@
+// 87. Check if number is even or odd
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num % 2 == 0)
+        printf("Even");
+    else
+        printf("Odd");
+    return 0;
+}
