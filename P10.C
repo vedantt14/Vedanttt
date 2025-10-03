386eeb0a19ed25caa45e9e31ccc9fb604ad15a97
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 15:37:55 +0530

    Create P10.C

---
+++ P10.C
@@ -0,0 +1,9 @@
+// Program 10: Add two float numbers from user
+#include <stdio.h>
+int main() {
+    float a, b;
+    printf("Enter two float numbers: ");
+    scanf("%f %f", &a, &b);
+    printf("Sum = %.2f", a + b);
+    return 0;
+}
