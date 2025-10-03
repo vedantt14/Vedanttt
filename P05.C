bab82abc82fd31c6fb654dfe3afe80c09e7fd747
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 15:35:30 +0530

    Create P05.C

---
+++ P05.C
@@ -0,0 +1,9 @@
+// Program 5: Add 5 user-entered integers
+#include <stdio.h>
+int main() {
+    int a, b, c, d, e;
+    printf("Enter 5 numbers: ");
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    printf("Sum = %d", a + b + c + d + e);
+    return 0;
+}
