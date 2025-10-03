c2fc601dc76dbfe1225b11f607f397a16ef7ee19
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 15:48:51 +0530

    Create P31.C

---
+++ P31.C
@@ -0,0 +1,8 @@
+// Program 31: Celsius to Fahrenheit
+#include <stdio.h>
+int main() {
+    float celsius = 25;
+    float fahrenheit = (celsius * 9 / 5) + 32;
+    printf("Fahrenheit = %.2f", fahrenheit);
+    return 0;
+}
