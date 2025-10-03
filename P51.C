c862f5d62b7149e7fc84e6645e7b18213266f38c
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:00:12 +0530

    Create P51.C

---
+++ P51.C
@@ -0,0 +1,8 @@
+// Program 51: Simple Interest
+#include <stdio.h>
+int main() {
+    float p = 1000, r = 5, t = 2;
+    float si = (p * r * t) / 100;
+    printf("Simple Interest = %.2f", si);
+    return 0;
+}
