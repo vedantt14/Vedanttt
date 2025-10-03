de718518d46e9b2dbb48fbbebc4c9747bad29c1a
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 15:51:12 +0530

    Create P36.C

---
+++ P36.C
@@ -0,0 +1,8 @@
+// Program 36: Feet to Meters
+#include <stdio.h>
+int main() {
+    float feet = 10;
+    float meters = feet / 3.28084;
+    printf("%.2f feet = %.2f meters", feet, meters);
+    return 0;
+}
