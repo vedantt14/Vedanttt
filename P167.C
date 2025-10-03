0de0beb028b5ad56252fa1d43c3c48fe4d3ca0fd
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 22:09:25 +0530

    Create P167.C

---
+++ P167.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n, sum = 0;
+    scanf("%d", &n);
+    for (int i = 1; i < n; i++) {
+        if (n % i == 0) sum += i;
+    }
+    if (sum == n)
+        printf("Perfect number");
+    else
+        printf("Not a perfect number");
+    return 0;
+}
