e032e7acef980eb164685a766adfcd1ee9ff7454
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 15:56:03 +0530

    Create P46.C

---
+++ P46.C
@@ -0,0 +1,9 @@
+// Program 46: Hexadecimal to Decimal
+#include <stdio.h>
+int main() {
+    int decimal;
+    printf("Enter hexadecimal: ");
+    scanf("%x", &decimal);
+    printf("Decimal = %d", decimal);
+    return 0;
+}
