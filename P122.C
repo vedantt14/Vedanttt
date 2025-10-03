439e7e2d6d4fcfd4e5f7015ab05502e83c7ad8c3
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:52:23 +0530

    Create P122.C

---
+++ P122.C
@@ -0,0 +1,10 @@
+// 22. Calculate simple interest
+#include <stdio.h>
+int main() {
+    float p, r, t, si;
+    printf("Enter principal, rate, time: ");
+    scanf("%f%f%f", &p, &r, &t);
+    si = (p * r * t) / 100;
+    printf("Simple Interest = %.2f", si);
+    return 0;
+}
