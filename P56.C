c548b7f2d9b83d5665dbe0781b543a10a1b76a4f
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:05:05 +0530

    Create P56.C

---
+++ P56.C
@@ -0,0 +1,6 @@
+// Program 56: Currency Conversion (USD to INR)
+#include <stdio.h>
+int main() {
+    float usd = 100, rate = 83.2;
+    float inr = usd * rate;
+    printf("%.2f USD = %.2f INR", usd, inr
