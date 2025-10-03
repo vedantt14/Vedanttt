2fc281e55b949fe9e2d6eb7461f6cc6f24218d92
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 15:39:07 +0530

    Create P12.C

---
+++ P12.C
@@ -0,0 +1,9 @@
+// Program 12: Subtract user input numbers
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Difference = %d", a - b);
+    return 0;
+}
