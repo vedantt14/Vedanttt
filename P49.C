4c6a8c4242ff64666aad9f2516d1e46d6897a6ef
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 15:59:21 +0530

    Create P49.C

---
+++ P49.C
@@ -0,0 +1,10 @@
+// Program 49: Check if number is even or odd
+#include <stdio.h>
+int main() {
+    int num = 7;
+    if(num % 2 == 0)
+        printf("Even");
+    else
+        printf("Odd");
+    return 0;
+}
