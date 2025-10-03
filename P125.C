bc78f95e59e949e08398a04e342a1f7cc86b2646
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:53:35 +0530

    Create P125.C

---
+++ P125.C
@@ -0,0 +1,9 @@
+// 25. Area of square
+#include <stdio.h>
+int main() {
+    float side;
+    printf("Enter side: ");
+    scanf("%f", &side);
+    printf("Area = %.2f", side * side);
+    return 0;
+}
