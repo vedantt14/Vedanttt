21be3ece67783bda4c07aed04794eb9375dcdea0
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:49:19 +0530

    Create P117.C

---
+++ P117.C
@@ -0,0 +1,9 @@
+// 17. Find square using input
+#include <stdio.h>
+int main() {
+    int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    printf("Square = %d", a * a);
+    return 0;
+}
