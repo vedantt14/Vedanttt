fb84de050a80afe64a6e361db664a70a8bc45b1e
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:35:15 +0530

    Create P96.C

---
+++ P96.C
@@ -0,0 +1,9 @@
+// 96. Calculate compound interest
+#include <stdio.h>
+#include <math.h>
+int main() {
+    float p = 1000, r = 5, t = 2;
+    float ci = p * pow((1 + r / 100), t) - p;
+    printf("Compound Interest = %.2f", ci);
+    return 0;
+}
