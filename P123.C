fd5a0b766cc81cedb3210814602c145a48326f1b
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:52:48 +0530

    Create P123.C

---
+++ P123.C
@@ -0,0 +1,9 @@
+// 23. Calculate area of rectangle
+#include <stdio.h>
+int main() {
+    float l, w;
+    printf("Enter length and width: ");
+    scanf("%f%f", &l, &w);
+    printf("Area = %.2f", l * w);
+    return 0;
+}
