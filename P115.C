6341a99aafbc461e441fec74b7bd7627a025b3e1
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:48:24 +0530

    Create P115.C

---
+++ P115.C
@@ -0,0 +1,9 @@
+// 15. Multiply two user input numbers
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Product = %d", a * b);
+    return 0;
+}
