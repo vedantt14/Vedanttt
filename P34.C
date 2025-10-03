27ba36285a047540df1e8a0bb4b1a5d1b8d92d56
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 15:50:16 +0530

    Create P34.C

---
+++ P34.C
@@ -0,0 +1,8 @@
+// Program 34: Miles to Kilometers
+#include <stdio.h>
+int main() {
+    float miles = 5;
+    float km = miles / 0.621371;
+    printf("%.2f miles = %.2f km", miles, km);
+    return 0;
+}
