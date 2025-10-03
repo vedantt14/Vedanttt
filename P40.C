6725d0667dca36062656e2ed85d73ba649d6c834
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 15:53:10 +0530

    Create P40.C

---
+++ P40.C
@@ -0,0 +1,8 @@
+// Program 40: Pounds to Kilograms
+#include <stdio.h>
+int main() {
+    float pounds = 154;
+    float kg = pounds / 2.20462;
+    printf("%.2f pounds = %.2f kg", pounds, kg);
+    return 0;
+}
