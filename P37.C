c28d88d2124da442c9275c9b5bc29fcb0da7bfe1
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 15:51:43 +0530

    Create P37.C

---
+++ P37.C
@@ -0,0 +1,8 @@
+// Program 37: Centimeters to Inches
+#include <stdio.h>
+int main() {
+    float cm = 50;
+    float inches = cm / 2.54;
+    printf("%.2f cm = %.2f inches", cm, inches);
+    return 0;
+}
