07482566ef1bdb21f84e850e9ac5f2d9caaf3d02
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:31:55 +0530

    Create P88.C

---
+++ P88.C
@@ -0,0 +1,12 @@
+// 88. Check if number is positive or negative
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num >= 0)
+        printf("Positive");
+    else
+        printf("Negative");
+    return 0;
+}
