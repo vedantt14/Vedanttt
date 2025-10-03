affc87f4bb7614bbbf08b265ff7558549e6b7006
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:11:32 +0530

    Create P69.C

---
+++ P69.C
@@ -0,0 +1,9 @@
+// Program 72: Print even numbers from 1 to 20
+#include <stdio.h>
+int main() {
+    for(int i = 1; i <= 20; i++) {
+        if(i % 2 == 0)
+            printf("%d ", i);
+    }
+    return 0;
+}
