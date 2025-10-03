61d74423aed8efdd0de99f6cf6a132e7d4e6f62c
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 17:00:36 +0530

    Create P140.C

---
+++ P140.C
@@ -0,0 +1,9 @@
+// 40. Convert kilometers to meters
+#include <stdio.h>
+int main() {
+    float km;
+    printf("Enter distance in km: ");
+    scanf("%f", &km);
+    printf("Meters = %.2f", km * 1000);
+    return 0;
+}
