7ff10437fc4ac3f5810bd107794ac1a51244bce7
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:56:51 +0530

    Create P132.C

---
+++ P132.C
@@ -0,0 +1,12 @@
+// 32. Find even or odd
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if(num % 2 == 0)
+        printf("Even");
+    else
+        printf("Odd");
+    return 0;
+}
