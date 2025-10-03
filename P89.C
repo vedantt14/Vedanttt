0913905952d81e8cdd0db1c0f76f19ac3cbdf433
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:32:20 +0530

    Create P89.C

---
+++ P89.C
@@ -0,0 +1,10 @@
+// 89. Find maximum of two numbers
+#include <stdio.h>
+int main() {
+    int a = 20, b = 30;
+    if (a > b)
+        printf("Max = %d", a);
+    else
+        printf("Max = %d", b);
+    return 0;
+}
