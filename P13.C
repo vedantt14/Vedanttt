dc583c422f257494721d22c81f5369a7a6073181
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 15:39:34 +0530

    Create P13.C

---
+++ P13.C
@@ -0,0 +1,8 @@
+// Program 13: Subtract three numbers
+#include <stdio.h>
+int main() {
+    int a = 20, b = 5, c = 3;
+    int result = a - b - c;
+    printf("Result = %d", result);
+    return 0;
+}
