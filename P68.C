ff8f56dafb116ee0962520b56222943e81ab9ef9
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:11:08 +0530

    Create P68.C

---
+++ P68.C
@@ -0,0 +1,10 @@
+// Program 71: Print numbers from 1 to 10 using while loop
+#include <stdio.h>
+int main() {
+    int i = 1;
+    while(i <= 10) {
+        printf("%d ", i);
+        i++;
+    }
+    return 0;
+}
