499324488ba961a7d7ab9855ff0ffbc1f251d497
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:57:30 +0530

    Create P133.C

---
+++ P133.C
@@ -0,0 +1,12 @@
+// 33. Positive or negative
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if(num >= 0)
+        printf("Positive");
+    else
+        printf("Negative");
+    return 0;
+}
