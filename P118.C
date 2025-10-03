22127b34b52c59586f3a824bebfa807bad191562
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:49:44 +0530

    Create P118.C

---
+++ P118.C
@@ -0,0 +1,9 @@
+// 18. Find cube using input
+#include <stdio.h>
+int main() {
+    int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    printf("Cube = %d", a * a * a);
+    return 0;
+}
