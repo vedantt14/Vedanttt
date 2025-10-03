439ce1d28b1cb120d8964cb72ca62c17085493a3
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 15:42:19 +0530

    Create P17.C

---
+++ P17.C
@@ -0,0 +1,9 @@
+// Program 17: Multiply user input numbers
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Product = %d", a * b);
+    return 0;
+}
