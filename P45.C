0e376895226116bccff4277580f7f996148934c5
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 15:55:35 +0530

    Create P45.C

---
+++ P45.C
@@ -0,0 +1,9 @@
+// Program 45: Octal to Decimal
+#include <stdio.h>
+int main() {
+    int octal;
+    printf("Enter octal number: ");
+    scanf("%o", &octal);
+    printf("Decimal = %d", octal);
+    return 0;
+}
