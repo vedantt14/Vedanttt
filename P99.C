6dcced6db1ddcc8b7f88a54dec735c2f97e4584a
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 16:36:28 +0530

    Create P99.C

---
+++ P99.C
@@ -0,0 +1,8 @@
+// 99. Calculate simple average of marks
+#include <stdio.h>
+int main() {
+    int m1 = 75, m2 = 80, m3 = 70;
+    float avg = (m1 + m2 + m3) / 3.0;
+    printf("Average = %.2f", avg);
+    return 0;
+}
