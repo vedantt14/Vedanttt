5ca88d601985582b9cd919ef15cc45a481c4f341
vedantt14 <VEDANTLOHAKARE232@GMAIL.COM>
2025-10-03 15:52:18 +0530

    Create P38.C

---
+++ P38.C
@@ -0,0 +1,8 @@
+// Program 38: Inches to Centimeters
+#include <stdio.h>
+int main() {
+    float inches = 20;
+    float cm = inches * 2.54;
+    printf("%.2f inches = %.2f cm", inches, cm);
+    return 0;
+}
